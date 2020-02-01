564213e619fc619705881e4b330e89ccfcb84c72
pokepetter <pokepetter@gmail.com>
2020-02-01 21:52:42 +0100

    made dentist scene camptible with scene switching.

--- main.py
+++ main.py
@@ -2,33 +2,40 @@ from ursina import *
 from dentist import JawMinigame
 
 
-app = Ursina()
 
-window.color = color._32
-window.fps_counter.enabled = False
-window.exit_button.visible = False
-Text.default_resolution *= 4
-Text.default_font = 'monogram_extended.ttf'
-Text.size *= 2
+class DirtyDentist(Entity):
+    def __init__(self):
+        super().__init__(self, eternal=True)
+        self.scene = None
+        self.scene_1 = JawMinigame(parent=self, enabled=False)
 
 
-dentist_minigame = JawMinigame()
-dentist_minigame.stop(False)
-dentist_minigame.enabled = False
-
-next_scene = Entity(model='cube', color=color.red, enabled=False)
-dentist_minigame.next_scene = next_scene
-
-
-
-def input(key):
-    global dentist_minigame
-    if key == 'j':
+    def go_to_scene(self, value):
+        print('go to scene:', value)
         camera.overlay.fade_in(duration=1)
+        self.scene = value
+        invoke(setattr, self.scene, 'enabled', True, delay=1)
         camera.overlay.fade_out(duration=1, delay=1.1)
-        print('lol')
-        invoke(setattr, dentist_minigame, 'enabled', True, delay=1)
-        invoke(dentist_minigame.start, delay=1)
 
 
-app.run()
+
+    def input(self, key):
+        if key == '1':
+            self.go_to_scene(BeatEmUp)
+        if key == '2':
+            self.go_to_scene(self.scene_1)
+
+
+
+
+if __name__ == '__main__':
+    app = Ursina()
+    window.color = color._32
+    window.fps_counter.enabled = False
+    window.exit_button.visible = False
+    Text.default_resolution *= 4
+    Text.default_font = 'monogram_extended.ttf'
+    Text.size *= 2
+
+    DirtyDentist()
+    app.run()
