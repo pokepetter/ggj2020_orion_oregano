072587afb75b5e8593b52cf925ff51456b216ee7
GuMMaN <trygve@igesund.com>
2020-02-01 21:24:36 +0100

    Make beat-em-up a class

--- beat_em_up.py
+++ beat_em_up.py
@@ -3,122 +3,125 @@ from enemy import Enemy
 from player import Player
 from dialogue_box import DialogueBox
 
+class BeatEmUp(Entity):
 
-window.set_z_order(window.Z_top)
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.ui = Entity(parent=camera.ui)
+        window.color = color.light_gray
+        camera.orthographic = True
+        camera.fov = 20
 
-app = Ursina()
-window.color = color.light_gray
-camera.orthographic = True
-camera.fov = 20
+        self.player_top_constraint = 3
+        self.player_bottom_constraint = -5
 
-player_top_constraint = 3
-player_bottom_constraint = -5
+        self.current_street = 1
 
-current_street = 1
-
-enemies = []
-
-
-asphalt = Entity(
-    model = 'quad',
-    color = color.light_gray.tint(-.4),
-    z = 0,
-    y = 3,
-    origin_y = .5,
-    scale = (1000, 8, 10),
-    collider = 'box',
-    ignore = True,
-    )
-
-below_ground = Entity(
-    model = 'quad',
-    color = color.olive.tint(-.4),
-    z = 0,
-    y = -5,
-    origin_y = .5,
-    scale = (1000, 8, 10),
-    collider = 'box',
-    ignore = True,
-    )
-
-player = Player()
-
-dialogue = DialogueBox([
-            ["Tooth criminal", "Omg"],
-            ["Tooth criminal", "There are people here"],
-            ["Tooth criminal", "I gotta..\nI gotta knock their <red>teeth <default>out!"]
-        ], parent=camera.ui)
+        self.enemies = []
 
 
-def spawn_random_enemies(amount):
-    for i in range(amount):
-        enemies.append(Enemy(
-            x = random.randint(-10, 10),
-            y = random.randint(-3, 2)))
-spawn_random_enemies(3)
+        self.asphalt = Entity(
+            model = 'quad',
+            color = color.light_gray.tint(-.4),
+            z = 0,
+            y = 3,
+            origin_y = .5,
+            scale = (1000, 8, 10),
+            collider = 'box',
+            ignore = True,
+            )
+
+        self.below_ground = Entity(
+            model = 'quad',
+            color = color.olive.tint(-.4),
+            z = 0,
+            y = -5,
+            origin_y = .5,
+            scale = (1000, 8, 10),
+            collider = 'box',
+            ignore = True,
+            )
+
+        self.player = Player()
+
+        self.dialogue = DialogueBox([
+                    ["Tooth criminal", "Omg"],
+                    ["Tooth criminal", "There are people here"],
+                    ["Tooth criminal", "I gotta..\nI gotta knock their <red>teeth <default>out!"]
+                ], parent=camera.ui)
+
+        self.spawn_random_enemies(3)
 
 
-def go_to_next_street():
-    global enemies, current_street
-    player.x = -(camera.fov * camera.aspect_ratio / 2 - player.scale_x)
-    for enemy in enemies:
-        destroy(enemy)
-    enemies = []
-    spawn_random_enemies(3)
-    current_street += 1
 
-
-def update():
-    if not dialogue:
-        player_controls()
-        enemies_chase_player()
-    if player.x >= (camera.fov * camera.aspect_ratio / 2 - player.scale_x) and enemies==[]:
-        go_to_next_street()
-
-def player_controls():
-    if player.x < (camera.fov * camera.aspect_ratio / 2 - player.scale_x):
-        player.x += held_keys["d"] * time.dt * player.speed
-    if player.x > -(camera.fov * camera.aspect_ratio / 2 - player.scale_x):
-        player.x -= held_keys["a"] * time.dt * player.speed
-    if player.y < player_top_constraint:
-        player.y += held_keys["w"] * time.dt * player.speed
-    if player.y > (player_bottom_constraint + player.scale_y/2):
-        player.y -= held_keys["s"] * time.dt * player.speed
-
-def enemies_chase_player():
-    for enemy in enemies:
-        if enemy.hp < enemy.max_hp:
-            enemy.position = lerp(enemy.position, player.position, enemy.speed * time.dt)
-            if distance2d(player.position, enemy.position) < 1:
-                player.hp -= enemy.attack()
-
-
-def input(key):
-    if dialogue and key == "space":
-        dialogue.current_text += 1
-
-    if not dialogue and key == "space":
-        for enemy in enemies:
-            if distance2d(player.position, enemy.position) < 1:
-                enemy.hp = enemy.hp - player.punch_power
-                if(enemy.hp <= 0):
-                    enemies.remove(enemy)
-                    destroy(enemy)
-
-    if key == "t":
-        print(time.time())
+    def spawn_random_enemies(self, amount):
+        for i in range(amount):
+            self.enemies.append(Enemy(
+                x = random.randint(-10, 10),
+                y = random.randint(-3, 2)))
 
 
 
 
-window.size = (window.fullscreen_size[0]//2, window.fullscreen_size[1]//2)
-window.position = (int(window.size[0]), int(window.size[1]-(window.size[1]/2)))
-window.borderless = False
-window.fullscreen = False
+    def go_to_next_street(self):
+        global enemies, current_street
+        self.player.x = -(camera.fov * camera.aspect_ratio / 2 - self.player.scale_x)
+        for enemy in self.enemies:
+            destroy(enemy)
+        self.enemies = []
+        self.spawn_random_enemies(3)
+        self.current_street += 1
 
-input_handler.bind('right arrow', 'd')
-input_handler.bind('left arrow', 'a')
-input_handler.bind('up arrow', 'w')
-input_handler.bind('down arrow', 's')
 
-app.run()
+    def update(self):
+        if not self.dialogue:
+            self.player_controls()
+            self.enemies_chase_player()
+        if self.player.x >= (camera.fov * camera.aspect_ratio / 2 - self.player.scale_x) and self.enemies==[]:
+            self.go_to_next_street()
+
+    def player_controls(self):
+        if self.player.x < (camera.fov * camera.aspect_ratio / 2 - self.player.scale_x):
+            self.player.x += held_keys["d"] * time.dt * self.player.speed
+        if self.player.x > -(camera.fov * camera.aspect_ratio / 2 - player.scale_x):
+            self.player.x -= held_keys["a"] * time.dt * self.player.speed
+        if self.player.y < self.player_top_constraint:
+            self.player.y += held_keys["w"] * time.dt * self.player.speed
+        if self.player.y > (self.player_bottom_constraint + self.player.scale_y/2):
+            self.player.y -= held_keys["s"] * time.dt * self.player.speed
+
+    def enemies_chase_player(self):
+        for enemy in self.enemies:
+            if enemy.hp < enemy.max_hp:
+                enemy.position = lerp(enemy.position, self.player.position, enemy.speed * time.dt)
+                if distance2d(self.player.position, enemy.position) < 1:
+                    self.player.hp -= enemy.attack()
+
+
+    def input(self, key):
+        if self.dialogue and key == "space":
+            self.dialogue.current_text += 1
+
+        if not self.dialogue and key == "space":
+            for enemy in self.enemies:
+                if distance2d(self.player.position, enemy.position) < 1:
+                    enemy.hp = enemy.hp - self.player.punch_power
+                    if(enemy.hp <= 0):
+                        self.enemies.remove(enemy)
+                        destroy(enemy)
+
+    def end(self):
+        
+
+
+
+#
+# window.size = (window.fullscreen_size[0]//2, window.fullscreen_size[1]//2)
+# window.position = (int(window.size[0]), int(window.size[1]-(window.size[1]/2)))
+# window.borderless = False
+# window.fullscreen = False
+
+# input_handler.bind('right arrow', 'd')
+# input_handler.bind('left arrow', 'a')
+# input_handler.bind('up arrow', 'w')
+# input_handler.bind('down arrow', 's')
