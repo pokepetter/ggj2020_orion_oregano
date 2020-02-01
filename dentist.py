f52f824530d38a31499eb64ebf6c63944c715d12
pokepetter <pokepetter@gmail.com>
2020-02-01 20:48:14 +0100

    started on scene transition.

--- dentist.py
+++ dentist.py
@@ -6,7 +6,11 @@ from copy import copy
 class JawMinigame(Entity):
 
     def __init__(self, **kwargs):
-        super().__init__()
+        super().__init__(**kwargs)
+
+        self.started = False
+        self.next_scene = None
+
         self.ui = Entity(parent=camera.ui)
         self.editor_camera = EditorCamera(parent=self, rotate_around_mouse_hit=False, y=1)
         self.editor_camera.rotation_x = 20
@@ -14,11 +18,14 @@ class JawMinigame(Entity):
         self.bg = Sky(
             parent=self,
             texture='brick',
-            color=color.lime,
+            color=color.lime.tint(-.35),
             # texture_scale=(64,32)
             )
         self.bg.texture.filtering = None
         self.animate_background_color()
+
+        self.music = Audio('dentist', parent=self, repeat=True, autoplay=False)
+        # self.music.volume = 1
         # self.floor = Entity(parent=self, model='plane', y=-8, scale=100, texture='white_cube', texture_scale=(64,64))
         # self.bg = Entity(model='quad', z=100, scale=1000, color=color.black)
         # self.bg.look_at(camera, 'back')
@@ -28,6 +35,7 @@ class JawMinigame(Entity):
         self.jaw_model = Entity(parent=self, model='gum', enabled=False)
 
         self.jaw = self.generate_jaw()
+        self.jaw.x = 20
         self.randomize_jaw(self.jaw)
         self.finished_jaw = self.generate_jaw()
         self.finished_jaw.x = -20
@@ -155,7 +163,6 @@ class JawMinigame(Entity):
             if random.random() < .1:
                 tooth_slot.tooth.color = color.gold
 
-
             if random.random() < .5:
                 tooth_slot.tooth.enabled = False
                 tooth_slot.collision = True
@@ -179,6 +186,7 @@ class JawMinigame(Entity):
             particle.y += .1
             particle.animate_y(particle.y+1, duration=2, curve=curve.linear)
             particle.fade_out(delay=.2, duration=.2)
+            Audio('placetooth', pitch=random.uniform(.8,1.1), volume=random.uniform(.8,1))
             self.earnings += 100
             self.check_for_win()
 
@@ -223,13 +231,12 @@ class JawMinigame(Entity):
 
 
     def update(self):
+        if not self.started:
+            return
+
         if self.time <= 0:
             if not self.out_of_time:
-                # destroy(self.cursor)
-                camera.overlay.fade_in(delay=.5, duration=1)
-                self.tools_menu.animate_x((.5*camera.aspect_ratio)+.05, duration=.25, curve=curve.in_out_expo)
-                destroy(self.ui, delay=1.5)
-                destroy(self)
+                self.stop()
 
             self.out_of_time = True
             return
@@ -247,18 +254,39 @@ class JawMinigame(Entity):
         self.timer.text = ("%1f" % self.time)[:5]
 
 
-    def on_enable(self):
-        self.ui.enabled = True
+    def start(self):
+        print('start')
+        self.jaw.x = 20
+        self.jaw.animate_x(0, duration=1)
+        self.time = 30
+        self.timer.scale = 2
+        self.timer.color = color.smoke
+        self.earnings = 0
+        invoke(setattr, self.ui, 'enabled', True, delay=0)
+        invoke(setattr, self.cursor, 'enabled', True, delay=1)
+        invoke(self.music.play, delay=1)
+        invoke(setattr, self, 'started', True, delay=1)
 
-    def on_disable(self):
-        self.ui.enabled = False
+
+    def stop(self, go_to_next_scene=True):
+        self.cursor.enabled = False
+        self.ui.enabled=False
+
+        if self.next_scene:
+            camera.overlay.fade_in(delay=1, duration=1)
+            invoke(setattr, self, 'enabled', False, delay=2)
+            invoke(setattr, self.next_scene, 'enabled', True, delay=2)
+            camera.overlay.fade_out(delay=2.1, duration=1)
+            self.music.fade_out(duration=1, destroy_on_ended=False)
+
 
 
     def animate_background_color(self):
         # self.bg.color=color.random_color().tint(-.5)
         step = 1/150*128
-        self.bg.animate_color(color.random_color().tint(-.25), curve=curve.linear, duration=step)
-        invoke(self.animate_background_color, delay=step)
+        if self.started:
+            self.bg.animate_color(color.random_color().tint(-.25), curve=curve.linear, duration=step)
+            invoke(self.animate_background_color, delay=step)
 
 
 if __name__ == '__main__':
@@ -270,6 +298,12 @@ if __name__ == '__main__':
     Text.size *= 2
 
     jaw_game = JawMinigame()
+    # jaw_game.enabled = False
+    jaw_game.stop()
+    jaw_game.start()
 
+    def input(key):
+        if key == 'space':
+            jaw_game.enabled = True
 
     app.run()
